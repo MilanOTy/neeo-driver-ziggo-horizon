cf6a6cc8e988aaa38e78998453a53a4d3bfbff0c
Gilles van den Hoven <gilles@webunity.nl>
2019-02-20 12:31:21 +0100

    Removed Neeo Helper and extracted it to a seperate repository.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 const NeeoSdk = require('neeo-sdk');
 
 // Supporting debug modules
-const NeeoHelper = require('./lib/NeeoHelper');
-const Helper = new NeeoHelper('ziggo-horizon:main');
+const NeeoSdkWebunity = require('neeo-sdk-webunity');
+const NeeoSdkHelper = new NeeoSdkWebunity('ziggo-horizon:main');
 
 // Start discovery object
 const Manager = require('./lib/manager');
@@ -41,16 +41,16 @@ const horizonController = NeeoSdk.buildDevice('Horizon Mediabox XL')
 	.addButtonGroup('Transport Search')
 	.addButtonGroup('Record')
 	.addButtonGroup('Color Buttons')
-	.addButton({ name: 'GUIDE', label: Helper.ConfigHasOr('ziggoHorizon.UiLabels.Guide', 'TV Guide') })
-	.addButton({ name: 'ONDEMAND', label: Helper.ConfigHasOr('ziggoHorizon.UiLabels.OnDemand', 'On Demand') })
-	.addButton({ name: 'HELP', label: Helper.ConfigHasOr('ziggoHorizon.UiLabels.Help', 'Help') })
-	.addButton({ name: 'INFO', label: Helper.ConfigHasOr('ziggoHorizon.UiLabels.Info', 'Info') })
-	.addButton({ name: 'TEXT', label: Helper.ConfigHasOr('ziggoHorizon.UiLabels.Text', 'Text') })
+	.addButton({ name: 'GUIDE', label: NeeoSdkHelper.ConfigHasOr('ziggoHorizon.UiLabels.Guide', 'TV Guide') })
+	.addButton({ name: 'ONDEMAND', label: NeeoSdkHelper.ConfigHasOr('ziggoHorizon.UiLabels.OnDemand', 'On Demand') })
+	.addButton({ name: 'HELP', label: NeeoSdkHelper.ConfigHasOr('ziggoHorizon.UiLabels.Help', 'Help') })
+	.addButton({ name: 'INFO', label: NeeoSdkHelper.ConfigHasOr('ziggoHorizon.UiLabels.Info', 'Info') })
+	.addButton({ name: 'TEXT', label: NeeoSdkHelper.ConfigHasOr('ziggoHorizon.UiLabels.Text', 'Text') })
 	.addButtonHandler((button, serialNumberOrUniqueId) => { MediaboxManager.ButtonPressed(serialNumberOrUniqueId, button); })
 	.registerFavoriteHandlers({ execute: (serialNumberOrUniqueId, favoriteId) => { MediaboxManager.FavoritePressed(serialNumberOrUniqueId, favoriteId); } })
 	.enableDiscovery({
-		  headerText: Helper.ConfigHasOr('ziggoHorizon.Discovery.Header', 'Prepare your mediabox(es)'),
-		  description: Helper.ConfigHasOr('ziggoHorizon.Discovery.Description', 'Make sure the Mediaboxes you want to discover are connected to your home network.')
+		  headerText: NeeoSdkHelper.ConfigHasOr('ziggoHorizon.Discovery.Header', 'Prepare your mediabox(es)'),
+		  description: NeeoSdkHelper.ConfigHasOr('ziggoHorizon.Discovery.Description', 'Make sure the Mediaboxes you want to discover are connected to your home network.')
 		}, getDevicesForNeeoWrapper);
 
 MediaboxManager.GetDevicesForNeeo();
